cf2e462e4e9b32f0022ec9a79b4f5aedd5d57af7
D N <danuser2018@gmail.com>
2022-10-31 19:37:02 +0100

    Changing to java

---
+++ src/main/java/me/danuser2018/peer/node/PeerNodeApplication.java
@@ -0,0 +1,13 @@
+package me.danuser2018.peer.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/danuser2018/peer/node/PeerNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.danuser2018.peer.node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
